2bfb73d1f2b680f3ce42bb673042aab789f920ff
Juan Larrotta <juanlarrotta@habi.co>
2024-03-07 16:15:07 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { TextField } from "@mui/material";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h2>Home Juan Larrotta</h2>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </>
+  );
+}
